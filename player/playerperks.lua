5f17b1b4137b25e9f11a58013b25b55f7d49c359
FrancescoMagliocco <Magliocco.W.Francesco@Gmail.com>
2021-02-19 16:26:49 -0500

    Added some documentation

--- player/playerperks.lua
+++ player/playerperks.lua
@@ -15,10 +15,21 @@ local CheckPerk = function(funcName, perkName)
     return perk
 end
 
+-- perkName is always converted to lowercase, then stirpped down of any
+-- character that isn't alphanumeric.  You could specify "CoLDBLoD", "C O l d b
+-- l ood", "C_O_LD     BLOOD" and the mod will still understand that you are
+-- refering to the Perk "ColdBlood"
+
+-- Returns a Perk of name perkName.  If no perk exists with perkName, nil is
+-- returned.
 function PlayerPerks.Get(perkName)
     return _Perks[perkName:lower():gsub("%W", "")]
 end
 
+-- Returns a Perk that is represented internally by perkIndex.  If no Perk is
+-- represented by perkIndex internally, nil is returned.
+--
+-- perkIndex is zero based.
 function PlayerPerks.GetPerkFromIndex(perkIndex)
     local perkTable = gamedataPerkType.GetPerkTableFromIndex(perkIndex)
 
@@ -30,41 +41,65 @@ function PlayerPerks.GetPerkFromIndex(perkIndex)
     return CheckPerk("PlayerPerks.GetPerkFromIndex():", perkTable[1])
 end
 
+-- If perkName is a valid perk, current level of perkName is returned.  If no
+-- perk exists for perkName, nil is returned.
 function PlayerPerks.GetPerkLevel(perkName)
     local perk = CheckPerk("PlayerPerks.GetPerkLevel():", perkName)
     return perk and perk:GetCurLevel()
 end
 
+-- If perkName is a valid perk, the level of perkName is increased.  It is not
+-- recommended to use this function to increase the level of a perk.  There are
+-- certain packages that are applied per Perk.  If you want to increase the
+-- level of a Perk, PlayerPerks.BuyPerk(perkName) will do exactly that and add
+-- any packages that come along with the Perk.
 function PlayerPerks.IncreasePerkLevel(perkName)
     local perk = CheckPerk("PlayerPerks.IncreasePerkLevel():", perkName)
     return perk and perk:IncreaseLevel()
 end
 
+-- If perkName is a valid Perk, returns the max level of Perk perkName. If
+-- perkName is not a valid Perk, nil is returned.
 function PlayerPerks.GetPerkMaxLevel(perkName)
     local perk = CheckPerk("PlayerPerks.GetPerkMaxLevel():", perkName)
     return perk and perk:GetMaxLevel()
 end
 
+-- If perkName is a valid Perk, the internal index of Perk is returned.  If
+-- perkName is not a valid Perk, nil is returned.
+--
+-- The index that is returned is unlike how Lua handles index; it is zero based.
+-- Most if not all functions in this mod, when asked for some type of index,
+-- index is zero based.  If there is a function that is not zero based, you can
+-- simply increment the index by one.
 function PlayerPerks.GetPerkIndex(perkName)
     local perk = CheckPerk("PlayerPerks.GetPerkIndex():", perkName)
     return perk and perk:GetIndex()
 end
 
+-- If perkName is a valid Perk, the name of Perk will be returned.
 function PlayerPerks.GetPerkName(perkName)
     local perk = CheckPerk("PlayerPerks.GetPerkName():", perkName)
     return perk and perk:GetName()
 end
 
+-- If perkName is a valid Perk, the reocrd for Perk perkName is returned.  If
+-- perkName is not a valid Perk, nil is returned.
 function PlayerPerks.GetPerkRecord(perkName)
     local perk = CheckPerk("PlayerPerks.GetPerkRecord():", perkName)
     return perk and perk:GetRecord()
 end
 
+-- If perkName is a valid Perk, the internal index of the PerkArea Perk perkName
+-- resides in is returned.  If perkName is not a valid Perk, nil is returned.
 function PlayerPerks.GetPerkAreaIndexForPerk(perkName)
     local perk = CheckPerk("PlayerPerks.GetPerkAreaForPerk():", perkName)
     return perk and Perk.GetPerkAreaIndex(perk:GetPerkArea())
 end
 
+-- XXX perkArea should probably be perkAreaName
+-- If perkArea is a valid PerkArea, the internal index of PerkArea perkArea is
+-- returned.  If perkArea is not a valid PerkArea, nil is returned.
 function PlayerPerks.GetPerkAreaIndex(perkArea)
     local perkAreaTable = gamedataPerkArea.Get(perkArea)
     if not perkAreaTable then
@@ -75,11 +110,18 @@ function PlayerPerks.GetPerkAreaIndex(perkArea)
     return perkAreaTable[2]
 end
 
+-- If perkName is a valid Perk, the Record for PerkArea Perk perkName resides in
+-- is returned.  If perkName is not a valid Perk, nil is returned.
 function PlayerPerks.GetPerkAreaRecordForPerk(perkName)
     local perk = CheckPerk("PlayerPerks.GetPerkAreaRecordForPerk():", perkName)
     return perk and perk:GetPerkAreaRecord()
 end
 
+
+-- XXX perkArea should probably be perkAreaName
+-- If perkArea is a valid PerkArea, the internal index of PerkArea perkArea is
+-- returned.  If perkArea is not a valid PerkArea, nil is returned.
+--
 -- I feel like this should be in the class perk...  But then again at the
 -- sametime, I think this is in the correct spot..
 function PlayerPerks.GetPerkAreaRecord(perkArea)
@@ -90,11 +132,30 @@ function PlayerPerks.GetPerkAreaRecord(perkArea)
     return Player.GetDevData():GetPerkAreaRecord(perkAreaIndex)
 end
 
+-- If perkName is a valid Perk, the PerkArea for which Perk perkName resides in
+-- will be unlocked and true will be returned.  If perkName is not a valid Perk,
+-- either nil or false is returned.
+--
+-- TODO Check if nil or false is returend
+--
+-- XXX Not sure if this actually works and if the requirments of the specific
+-- PerkArea still need to be met.
+-- XXX Does UnlockPerkArea() actually return anything?
 function PlayerPerks.UnlockPerkAreaForPerk(perkName)
     local perk = CheckPerk("PlayerPerks.UnlockPerkAreaForPerk():", perkName)
     return perk and perk:UnlockPerkArea()
 end
 
+-- If perkArea is a valid PerkArea, PerkArea perkArea will be unlocked and true
+-- will be returned.  If perkArea is not a valid PerkArea nil is returned.
+-- will be returned.
+--
+-- XXX Not sure if this actually works and if the requirments of PerkArea
+-- perkArea still need to be met.
+--
+-- We are using the internal functions to unlock a PerkArea, so we are returning
+-- true here unlike how in PlayerPerks.UnlockPerkAreaForPerk(), the returned
+-- value was the outcome of perk and perk:UnlockPerkArea()
 function PlayerPerks.UnlockPerkArea(perkArea)
     local perkAreaIndex = PlayerPerks.GetPerkAreaIndex(perkArea)
     if not perkAreaIndex then return end
@@ -102,11 +163,20 @@ function PlayerPerks.UnlockPerkArea(perkArea)
     return true
 end
 
+-- If perkName is a valid Perk, the PerkArea for which Perk perkName resides in
+-- will be locked and true will be returned.  If perkName is not a valid Perk,
+-- either false or nil is returned.
 function PlayerPerks.LockPerkAreaForPerk(perkName)
     local perk = CheckPerk("PlayerPerks.LockPerkAreaForPerk():", perkName)
     return perk and perk:LockPerkArea()
 end
 
+-- If perkArea is a valid PerkArea, PerkArea perkArea will be locked and true
+-- will be returned.  If perkArea is not a valid PerkArea, either nil is
+-- returned.
+--
+-- XXX Not sure if this actually works and if the requirements of PerkArea
+-- perkArea still need to be met.
 function PlayerPerks.LockPerkArea(perkArea)
     local perkAreaIndex = PlayerPerks.GetPerkAreaIndex(perkArea)
     if not perkAreaIndex then return end
@@ -114,6 +184,8 @@ function PlayerPerks.LockPerkArea(perkArea)
     return true
 end
 
+-- If perkName is a valid Perk, true or false is returned depending on if Player
+-- has Perk perkName.  If perkName is not a valid Perk, nil is retured.
 function PlayerPerks.HasPerk(perkName)
     local perk = CheckPerk("PlayerPerks.HasPerk():", perkName)
     -- We can't use the return approach similar to PlayerPerks.GetPerkMaxLevel()
@@ -123,11 +195,46 @@ function PlayerPerks.HasPerk(perkName)
     if perk then return perk:HasPerk() end
 end
 
+-- Given that perkName is a valid Perk and the following conditions are met:
+--  - PerkArea Perk perkName resides in is unlocked
+--  - Current level of Perk perkName isn't already max
+--  - Player has atleast one Primary Development Point or giveDevPoint is true
+-- If the above condtions are met and Player doesn't already have Perk perkName,
+-- Perk perkName will be added to Player.  If Player already has Perk perkName,
+-- the level of Perk perkName will be increased.  If a Perk was either added or
+-- had its level raised and giveDevPoint was not true, one Primary Development
+-- Point will be deducted.  If giveDevPoint is true, no Primary Development
+-- Points will be deducted.  After completion of adding or increaseing the level
+-- of Perk perkName and no error happend, true will be returned.  If an issues
+-- happend, false will be returned.  If perkName is not a valid Perk, nil will
+-- be returned.
+--
+-- XXX TODO Explain what giveDevPoint does
 function PlayerPerks.BuyPerk(perkName, giveDevPoint)
     local perk = CheckPerk("PlayerPerks.BuyPerk():", perkName)
     return perk and perk:BuyPerk(giveDevPoint)
 end
 
+-- TODO Add check to see if level is actually a number
+-- Given that perkName is a valid Perk and the following conditions are met:
+--  - level is not nil and not less than 0
+--  - Current level of Perk perkName is not already level
+--  - Current level of Perk perkName is < max level of Perk perkName
+-- If just one of the above conditions are not met, nil is returned.
+-- If all above condtions are met, if level is less than Perk perkName current
+-- level, Player should get back currenPerkLevel - level in Primary Development
+-- Points.  If there was no issue in modifying the level of Perk perkName, true
+-- is returned, otherwise nil is returned.
+--
+-- If level is greater than Perk perkName current level and Player either has at
+-- least level - currentPerkLevel in Primary Development Points if giveDevPoints
+-- is not true, true will be returned and if giveDevPoints was not true, level -
+-- currentPerlLevel in Primary Development Points will be deducted.  If
+-- giveDevPoints was true, no Primary Development Points will be deducted.  If
+-- Player doesn't have enough Primary Development Points and giveDevPoints is
+-- not true, nil will be retured.
+--
+-- XXX TODO Revise this documentation
 function PlayerPerks.SetPerkLevel(perkName, level, giveDevPoints)
     if not level or level < 0 then
         print("PlayerPerks.SetPerkLevel():", "Level must be > 0")
@@ -140,6 +247,7 @@ function PlayerPerks.SetPerkLevel(perkName, level, giveDevPoints)
         return
     end
 
+    -- XXX If Player doesn't have Perk perkName, is 0 returned for the level?
     -- We use perk current level a few times, so to reduce cpu cycles, we store
     -- it in memory
     local curLevel = perk:GetLevel()
@@ -189,16 +297,35 @@ function PlayerPerks.SetPerkLevel(perkName, level, giveDevPoints)
     return true
 end
 
+-- If perkName is a valid Perk, upon successful removeal of Perk perkName, true
+-- will be returned and Player should be rembursed by the correct amount of
+-- Primary Development Points.
+--
+-- I'm pretty sure that this will remove Perk perkName entirely, not just lower
+-- the level of Perk perkName.
+--
+-- If any issues occures or if perkName is not a valid Perk, nil is returned.
 function PlayerPerks.RemovePerk(perkName)
     local perk = CheckPerk("PlayerPerks.RemovePerk():", perkName)
     return perk and perk:RemovePerk()
 end
 
+-- If perkName is a valid Perk, true will be returned if the PerkArea Perk
+-- perkName resides in.  Otherwise false is returned.  If perkName is not a
+-- valid Perk, nil is returned.
+--
+-- TODO Revise documentaion a bit
 function PlayerPerks.IsPerkUnlocked(perkName)
     local perk = CheckPerk("PlayerPerk.IsPerkUnlocked():", perkName)
     if perk then return perk:IsPerkAreaUnlocked() end
 end
 
+-- TODO Maybe return true after Removeal of all Perks?
+-- Removes ALL Perks from Player.  Both areYouSure and areYouReallySure must be
+-- boolean true; not a number, not a string.  Only excepted value is true
+--
+-- If either of the two parameters are not true, nil will be returned, nil will
+-- still be returned even after all Perk's have been removed.
 function PlayerPerks.RemoveAllPerks(areYouSure, areYouReallySure)
     if areYouSure ~= true and areYouReallySure ~= true then return end
     Player.GetDevData():RemoveAllPerks()
